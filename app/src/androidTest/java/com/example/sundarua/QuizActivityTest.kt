e5aceebb0940dfffa8f6c94c7fe56f567c0e3122
herdialfachri <alfachri75@gmail.com>
2025-05-21 13:04:52 +0700

    running instrumented test for QuizActivityTest.kt

--- app/src/androidTest/java/com/example/sundarua/QuizActivityTest.kt
+++ app/src/androidTest/java/com/example/sundarua/QuizActivityTest.kt
@@ -1,68 +1,66 @@
 package com.example.sundarua
 
-import android.content.Context
-import android.content.SharedPreferences
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.*
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.intent.Intents.*
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.RootMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import com.example.sundarua.activity.MainActivity
 import com.example.sundarua.activity.QuizActivity
-import org.junit.After
-import org.junit.Before
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class QuizActivityTest {
 
-    private lateinit var sharedPref: SharedPreferences
+    // Delay helper
+    private fun delay(ms: Long = 3000L) {
+        Thread.sleep(ms)
+    }
 
     @Before
-    fun setUp() {
-        init()
-
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        sharedPref = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
-        sharedPref.edit()
-            .putString("user_name", "Test User")
-            .commit()
+    fun setupIntents() {
+        Intents.init()
     }
 
     @After
-    fun tearDown() {
-        release()
-
-        sharedPref.edit().clear().commit()
+    fun tearDownIntents() {
+        Intents.release()
     }
 
     @Test
-    fun testBackToMainActivityWithDelay() {
+    fun testBackButtonNavigatesToMainActivity() {
         ActivityScenario.launch(QuizActivity::class.java)
-        Thread.sleep(2000)
 
         onView(withId(R.id.back_main_btn)).perform(click())
-
-        Thread.sleep(2000)
+        delay()
 
         intended(hasComponent(MainActivity::class.java.name))
     }
 
     @Test
-    fun testClickQuizItem_ShowsDialog() {
+    fun testClickRecyclerViewItem_showsDialog() {
         ActivityScenario.launch(QuizActivity::class.java)
 
-        Thread.sleep(2000)
+        delay() // Tunggu data tampil
 
-        onView(withId(R.id.recycler_view))
-            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-
-        Thread.sleep(1000)
+        // Klik item pertama di RecyclerView
+        onView(withId(R.id.recycler_view)) // Ganti dengan ID RecyclerView-mu
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    0,
+                    click()
+                )
+            )
+        delay()
     }
 }
